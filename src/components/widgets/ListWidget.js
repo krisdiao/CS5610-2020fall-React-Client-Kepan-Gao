0e3970f9203218f31d1803674fbeb594438f0971
KrisGao <527369570@qq.com>
2020-12-01 18:44:12 -0500

    update list

--- src/components/widgets/ListWidget.js
+++ src/components/widgets/ListWidget.js
@@ -9,29 +9,32 @@ const ListWidget = ({
                              okWidget,
                              editWidget
                          }) =>
-
-    <div className="wbdv-light-grey-border">
-        <label className="pull-right">
-            <Switch
-                type="checkbox"
-                onChange={()=> editWidget(widget)}
-                checked={widget.editing}
-                color="primary"/>
-            Preview Off
-        </label>
-        <label className="pull-right">
-            <Switch
-                type="checkbox"
-                onChange={()=> okWidget(widget)}
-                checked={widget.editing}
-                color="primary"/>
-            Preview On
-        </label>
+{
+    const listItems = widget.text.split("\n")
+    return<div>
         {
-            widget.editing &&
-            <div>
-            <h3>List</h3>
-            <span className="pull-right">
+            <div className="wbdv-light-grey-border">
+                <label className="pull-right">
+                    <Switch
+                        type="checkbox"
+                        onChange={()=> editWidget(widget)}
+                        checked={widget.editing}
+                        color="primary"/>
+                    Preview Off
+                </label>
+                <label className="pull-right">
+                    <Switch
+                        type="checkbox"
+                        onChange={()=> okWidget(widget)}
+                        checked={widget.editing}
+                        color="primary"/>
+                    Preview On
+                </label>
+                {
+                    widget.editing &&
+                    <div>
+                        <h3>List</h3>
+                        <span className="pull-right">
                     <button
                         className="btn btn-success"
                         onClick={()=> okWidget(widget)}>
@@ -49,9 +52,9 @@ const ListWidget = ({
                 </button>
                 <select
                     onChange={(event)=> updateWidget({...widget,
-                    type:event.target.value})}
-                        value={widget.type}
-                        className="form-control-sm" >
+                        type:event.target.value})}
+                    value={widget.type}
+                    className="form-control-sm" >
                     <option value="HEADING">Heading</option>
                     <option value="PARAGRAPH">Paragraph</option>
                     <option value="LIST">List</option>
@@ -63,61 +66,80 @@ const ListWidget = ({
                     <i className="fa fa-times" aria-hidden="true"></i>
                 </button>
                 </span>
-            <br/>
-            <br/>
-            <textarea
-                rows="3"
-                cols="10"
-                onChange={(event)=> updateWidget({
-                    ...widget,
-                    text:event.target.value
-                })}
-                value={widget.text}
-                className="form-control">
+                        <br/>
+                        <br/>
+                        <textarea
+                            rows="3"
+                            cols="10"
+                            onChange={(event)=> updateWidget({
+                                ...widget,
+                                text:event.target.value
+                            })}
+                            value={widget.text}
+                            className="form-control">
             </textarea>
-            <br/>
-            <select
-                onChange={(event)=> updateWidget({...widget,
-                    value:event.target.value})}
-                value={widget.value}
-                className="form-control">
-                <option value="unordered">Unordered list</option>
-                <option value="ordered">Ordered list</option>
-            </select>
-            <br/>
-            <input
-                onChange={(event)=> updateWidget({
-                    ...widget,
-                    name:event.target.value
-                })}
-                value={widget.name}
-                className="form-control" />
-            <br/>
-            <h3>Preview</h3>
-                {
-                    widget.value === "unordered" &&
-                    <ul>{widget.text}</ul>
+                        <br/>
+                        <select
+                            onChange={(event)=> updateWidget({...widget,
+                                value:event.target.value})}
+                            value={widget.value}
+                            className="form-control">
+                            <option value="unordered">Unordered list</option>
+                            <option value="ordered">Ordered list</option>
+                        </select>
+                        <br/>
+                        <input
+                            onChange={(event)=> updateWidget({
+                                ...widget,
+                                name:event.target.value
+                            })}
+                            value={widget.name}
+                            className="form-control" />
+                        <br/>
+                        <h3>Preview</h3>
+                        {
+                            widget.value === "unordered" &&
+                            <div>
+                                {listItems.map(item =>
+                                <ul key={item}>{item}</ul>)}
+                            </div>
+
+                        }
+                        {
+                            widget.value === "ordered"&&
+                            <div>
+                                {listItems.map(item =>
+                                <li key={item}>{item}</li>)}
+                            </div>
+                        }
+                    </div>
                 }
                 {
-                    widget.value === "ordered"&&
-                    <li>{widget.text}</li>
+                    !widget.editing &&
+                    <div>
+                        <h3>Preview</h3>
+                        {
+                            widget.value === "unordered" &&
+                            <div>
+                                {listItems.map(item =>
+                                    <ul key={item}>{item}</ul>)}
+                            </div>
+
+                        }
+                        {
+                            widget.value === "ordered"&&
+                            <div>
+                                {listItems.map(item =>
+                                    <li key={item}>{item}</li>)}
+                            </div>
+                        }
+                    </div>
                 }
-            </div>
-        }
-        {
-            !widget.editing &&
-            <div>
-            <h3>Preview</h3>
-                {
-                    widget.value === "unordered" &&
-                    <ul>{widget.text}</ul>
-                }
-                {
-                    widget.value === "ordered"&&
-                    <li>{widget.text}</li>
-                }
-            </div>
-        }
+            </div>}
     </div>
+
+
+}
+
 
 export default ListWidget
